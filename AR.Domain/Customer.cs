6300cab8cc90faeaf7f24061b26b1fd8845b3d90
thiagoturazzi <thiago.turazzi@gmail.com>
2021-08-30 21:59:06 -0300

    Add Domain layer, created Customer entity and add Data layer, created MainContext and Customer Dbset

---
+++ AR.Domain/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AR.Domain
+{
+    public class Customer
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Field {0} is required")]
+        [MaxLength(50, ErrorMessage = "Field {0} should not exceed the max length {1}")]
+        public string Name { get; set; }
+        public string CPF { get; set; }
+        public int Age { get; set; }
+    }
+}
